dcf6144416220e31afec4356a6a7e073c4da612d
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-08-04 14:40:46 +0300

    hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react';
 import imagesApi from './components/imagesApi/imagesApi';
 import Searchbar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
@@ -7,75 +8,68 @@ import Button from './components/Button/Button';
 import Modal from './components/Modal/Modal';
 import Loader from 'react-loader-spinner';
 import './styles.css';
-class App extends Component {
-  state = {
-    hits: [],
-    currentPage: 1,
-    currentPageImages: [],
-    searchQuery: '',
-    isLoading: false,
-    error: null,
-    largeImage: '',
-    showModal: false,
-    modalUrl: '',
-    modalAlt: '',
+
+export default function App() {
+  const [hits, setHits]=useState([]);
+  const [currentPage, setCurrentPage]=useState(1);
+  const [currentPageImages, setCurrentPageImages]=useState([]);
+  const [searchQuery, setSearchQuery]=useState('');
+  const [isLoading, setIsLoading]=useState(false);
+  const [error, setError]=useState(null);
+  const [showModal, setShowModal]=useState(false); 
+  const [modalUrl, setModalUrl]=useState('');
+  const [modalAlt, setModalAlt]=useState('');
+
+  useEffect(()=>{
+    if (!searchQuery) {
+      return;
+      }
+    fetchHits();
+  },[searchQuery]);
+  const onChangeQuery = searchQuery => {
+    setSearchQuery(searchQuery);
+    setCurrentPage(1);
+    setHits([]);
+    setError(null)
+  }
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchHits();
-    }
-  } 
-  onChangeQuery = searchQuery => {
-    this.setState({ 
-      searchQuery: searchQuery, 
-      currentPage: 1, 
-      hits: [], 
-      error: null, 
-    })
-  }
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }))
-  }
-  fetchHits = () => {
-    const { searchQuery, currentPage } = this.state;
+  const fetchHits = () => {
+   // const { searchQuery, currentPage } = this.state;
     const options = { searchQuery, currentPage };
-    this.setState({ isLoading: true });
+    setIsLoading(true);
     imagesApi
     .fetchHits(options)
     .then(hits => {
-      this.setState( prevState => ({
-        hits: [...prevState.hits, ...hits], 
-        currentPage: prevState.currentPage + 1,
-        currentPageImages: [...hits], 
-      }));
-      if (hits.length === 0) {
-          this.setState({
-            error: 'Nothing was find by your query. Try again.',});
-      }})
+      setHits(prevHits => [...prevHits, ...hits]);
+      setCurrentPage(currentPage=>currentPage + 1);
+      setCurrentPageImages([...hits])
+        if (hits.length === 0) {
+          setError('Nothing was find by your query. Try again.');
+      };
+      })
       .then(() => {
                 window.scrollTo({
                   top: document.documentElement.scrollHeight,
                   behavior: 'smooth',
                 })
               })
-    .catch(error => this.setState({ error }))
-    .finally(() => this.setState({ isLoading: false }));
+    .catch(error => setError(error))
+    .finally(() => setIsLoading(false));
   };
-  onClickImageGalleryItem = e => {
-    this.setState({
-      modalUrl: e.currentTarget.getAttribute('url'),
-      modalAlt: e.currentTarget.getAttribute('alt'),
-    });
-    this.toggleModal();
+  const onClickImageGalleryItem = e => {
+    setModalUrl(e.currentTarget.getAttribute('url'));
+    setModalAlt(e.currentTarget.getAttribute('alt'));
+    toggleModal();
   };
-  render() {
-    const { hits, currentPageImages, isLoading, error, showModal, modalAlt, modalUrl } = this.state;
-    const shouldRenderLoadMoreButton = !(currentPageImages.length < 12) && !isLoading;
+  const shouldRenderLoadMoreButton = () => {
+    const should = !(currentPageImages.length < 12) && !isLoading;
+    return should;
+    } 
     return (
       <>
-        <Searchbar onChangeQuery={this.onChangeQuery} />
+        <Searchbar onChangeQuery={onChangeQuery} />
         {error && (
           <p >
            {error}
@@ -83,12 +77,10 @@ class App extends Component {
         )}
         <ImageGallery>
           {hits.map(({ id, tags, webformatURL, largeImageURL }) => (
-            <ImageGalleryItem key={id} alt={tags} src={webformatURL} url={largeImageURL} onClick={this.onClickImageGalleryItem} />
+            <ImageGalleryItem key={id} alt={tags} src={webformatURL} url={largeImageURL} onClick={onClickImageGalleryItem} />
           ))}
         </ImageGallery>
-
         {isLoading && 
-       
         <Loader 
         style={{textAlign: 'center'}}
         type="Puff"
@@ -97,14 +89,142 @@ class App extends Component {
         width={100}
         timeout={3000} //3 secs 
         /> }     
-                    
-        {shouldRenderLoadMoreButton && <Button  onfetchHits={this.fetchHits}/> }
-
+        {shouldRenderLoadMoreButton() && <Button  onfetchHits={fetchHits}/> }
         {showModal && (
-          <Modal src={modalUrl} alt={modalAlt} onClose={this.toggleModal} />
+          <Modal src={modalUrl} alt={modalAlt} onClose={toggleModal} />
         )}  
       </>      
     )    
-  }   
-};
-export default App;
+   }   
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// old code
+// class App extends Component {
+//   state = {
+//     hits: [],
+//     currentPage: 1,
+//     currentPageImages: [],
+//     searchQuery: '',
+//     isLoading: false,
+//     error: null,
+//     largeImage: '',
+//     showModal: false,
+//     modalUrl: '',
+//     modalAlt: '',
+//   };
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       this.fetchHits();
+//     }
+//   } 
+//   onChangeQuery = searchQuery => {
+//     this.setState({ 
+//       searchQuery: searchQuery, 
+//       currentPage: 1, 
+//       hits: [], 
+//       error: null, 
+//     })
+//   }
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal,
+//     }))
+//   }
+//   fetchHits = () => {
+//     const { searchQuery, currentPage } = this.state;
+//     const options = { searchQuery, currentPage };
+//     this.setState({ isLoading: true });
+//     imagesApi
+//     .fetchHits(options)
+//     .then(hits => {
+//       this.setState( prevState => ({
+//         hits: [...prevState.hits, ...hits], 
+//         currentPage: prevState.currentPage + 1,
+//         currentPageImages: [...hits], 
+//       }));
+//       if (hits.length === 0) {
+//           this.setState({
+//             error: 'Nothing was find by your query. Try again.',});
+//       }})
+//       .then(() => {
+//                 window.scrollTo({
+//                   top: document.documentElement.scrollHeight,
+//                   behavior: 'smooth',
+//                 })
+//               })
+//     .catch(error => this.setState({ error }))
+//     .finally(() => this.setState({ isLoading: false }));
+//   };
+//   onClickImageGalleryItem = e => {
+//     this.setState({
+//       modalUrl: e.currentTarget.getAttribute('url'),
+//       modalAlt: e.currentTarget.getAttribute('alt'),
+//     });
+//     this.toggleModal();
+//   };
+//   render() {
+//     const { hits, currentPageImages, isLoading, error, showModal, modalAlt, modalUrl } = this.state;
+//     const shouldRenderLoadMoreButton = !(currentPageImages.length < 12) && !isLoading;
+//     return (
+//       <>
+//         <Searchbar onChangeQuery={this.onChangeQuery} />
+//         {error && (
+//           <p >
+//            {error}
+//           </p>
+//         )}
+//         <ImageGallery>
+//           {hits.map(({ id, tags, webformatURL, largeImageURL }) => (
+//             <ImageGalleryItem key={id} alt={tags} src={webformatURL} url={largeImageURL} onClick={this.onClickImageGalleryItem} />
+//           ))}
+//         </ImageGallery>
+
+//         {isLoading && 
+       
+//         <Loader 
+//         style={{textAlign: 'center'}}
+//         type="Puff"
+//         color="#00BFFF"
+//         height={100}
+//         width={100}
+//         timeout={3000} //3 secs 
+//         /> }     
+                    
+//         {shouldRenderLoadMoreButton && <Button  onfetchHits={this.fetchHits}/> }
+
+//         {showModal && (
+//           <Modal src={modalUrl} alt={modalAlt} onClose={this.toggleModal} />
+//         )}  
+//       </>      
+//     )    
+//   }   
+// };
+// export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,35 +1,71 @@
-    import React, { Component } from "react";
-    class Searchbar extends Component{ 
-    state = { query: '' };
-
-    handleChange = element => {
-        this.setState({ query: element.currentTarget.value, })
-    };
-    handleSubmit=element=>{
-    element.preventDefault();
-        if (this.state.query.length !== 0) {
-            this.props.onChangeQuery(this.state.query)     
-        } 
+    import {useState} from 'react';
+    export default function Searchbar({onChangeQuery}){
+        const [query, setQuery] = useState('');
+        const handleChange = element => {
+                setQuery(element.currentTarget.value)
+            };
+        const handleSubmit=element=>{
+            element.preventDefault();
+                if (query.length !== 0) {
+                    onChangeQuery(query)     
+                } 
+            }
+          return (
+            <header className="Searchbar">
+                <form className="SearchForm" onSubmit={handleSubmit}>
+                    <button type="submit" onSubmit={handleSubmit} className="SearchForm-button">
+                        <span className="SearchForm-button-label">Search</span>
+                    </button>
+                    <input
+                        className="SearchForm-input"
+                        type="text"
+                        autoComplete="off"
+                        autoFocus
+                        placeholder="Search images and photos"
+                        value={query}
+                        onChange={handleChange}
+                    />
+                 </form>
+            </header>
+            );
     }
-    render(){
-        return (
-    <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-            <button type="submit" onSubmit={this.handleSubmit} className="SearchForm-button">
-                <span className="SearchForm-button-label">Search</span>
-            </button>
-            <input
-                className="SearchForm-input"
-                type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search images and photos"
-                value={this.state.query}
-                onChange={this.handleChange}
-            />
-         </form>
-    </header>
-    );
-  }
-}
-export default Searchbar;
+
+
+
+
+
+    // old code
+    // class Searchbar extends Component{ 
+    // state = { query: '' };
+
+    // handleChange = element => {
+    //     this.setState({ query: element.currentTarget.value, })
+    // };
+    // handleSubmit=element=>{
+    // element.preventDefault();
+    //     if (this.state.query.length !== 0) {
+    //         this.props.onChangeQuery(this.state.query)     
+    //     } 
+    // }
+//     render(){
+//         return (
+//     <header className="Searchbar">
+//         <form className="SearchForm" onSubmit={this.handleSubmit}>
+//             <button type="submit" onSubmit={this.handleSubmit} className="SearchForm-button">
+//                 <span className="SearchForm-button-label">Search</span>
+//             </button>
+//             <input
+//                 className="SearchForm-input"
+//                 type="text"
+//                 autoComplete="off"
+//                 autoFocus
+//                 placeholder="Search images and photos"
+//                 value={this.state.query}
+//                 onChange={this.handleChange}
+//             />
+//          </form>
+//     </header>
+//     );
+//   }
+// }
+// export default Searchbar;
