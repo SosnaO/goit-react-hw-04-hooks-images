6d4af12f98fddeb492efa81e027410db42c6c559
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-08-04 15:07:20 +0300

    modal hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import imagesApi from './components/imagesApi/imagesApi';
 import Searchbar from './components/Searchbar/Searchbar';
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,34 +1,53 @@
-import React, { Component} from 'react';
+import {useEffect} from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
-class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-    }
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown)
-    }
-    handleKeyDown = e => {
+export default function Modal({ onClose, src, alt }) {
+ const handleKeydown = e => {
         if (e.code === 'Escape') {
-            this.props.onClose();
+            onClose();
         }
-    }
-    handleBackdropClick = e => {
+    };
+    const handleBackdropClick = e => {
         if (e.currentTarget === e.target) {
-            this.props.onClose();
+            onClose();
         }
-    }
-    render(){
-        const { src, alt } = this.props;
-        return createPortal(
-            <div className="Overlay" onClick={this.handleBackdropClick}>
+    };
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeydown);
+
+        return () => window.removeEventListener('keydown', handleKeydown)
+     })
+       return createPortal(
+            <div className="Overlay" onClick={handleBackdropClick}>
                 <div className="Modal">
                     <img src={src} alt={alt} />
                 </div>
             </div>, 
             modalRoot,
         );
-    }
-}
-export default Modal;
+    
+};
+
+
+
+
+
+
+
+
+
+
+// old code
+
+// class Modal extends Component {
+//     componentDidMount() {
+//         window.addEventListener('keydown', this.handleKeyDown);
+//     }
+//     componentWillUnmount() {
+//         window.removeEventListener('keydown', this.handleKeyDown)
+//     }
+   
+   
+        // const { src, alt } = this.props;
+// export default Modal;
